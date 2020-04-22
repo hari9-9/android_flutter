ff31d3794d9851b4599fb567d7cabb168ce0ccf8
hari9_9 <reachk.hariharan@gmail.com>
2020-04-22 22:12:39 +0530

    Text styling

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -15,7 +15,20 @@ class Home extends StatelessWidget
           //height: 100.0,
           //margin:EdgeInsets.only(left:35.0,top: 50.0),
 
-          child: Text("Flight",textDirection: TextDirection.ltr,)
+          child: Text(
+            "Flight",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 75.0,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w300,
+              fontStyle: FontStyle.italic,
+              color: Colors.white
+
+            ),
+
+            )
     )
     );
   }
