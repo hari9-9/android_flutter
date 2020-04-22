e66174942abd1c6007ca7756f91974b5f723a964
hari9_9 <reachk.hariharan@gmail.com>
2020-04-22 20:33:53 +0530

    implementing a class to reduce code in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main()
 {
   runApp(
+    new MyflutterApp()
 
-		MaterialApp(
+		
+	);
+}
+
+class MyflutterApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
 			title: "My Flutter App",
 			home: Scaffold(
 				appBar: AppBar(title: Text("My First App Screen"),),
@@ -19,6 +29,7 @@ void main()
 					),
 				),
 			)
-		)
-	);
+		);
+  }
+
 }
