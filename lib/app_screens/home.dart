b06762ec10a27ec087f23c0f51ee05b043342e1a
hari9_9 <reachk.hariharan@gmail.com>
2020-04-23 16:24:01 +0530

    Row and expanded widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class Home extends StatelessWidget
-{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
     return Center(
-      child:Container(
-          alignment: Alignment.center,
-          color: Colors.deepPurple,
-          //width: 200.0,
-          //height: 100.0,
-          //margin:EdgeInsets.only(left:35.0,top: 50.0),
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            //width: 200.0,
+            //height: 100.0,
+            //margin:EdgeInsets.only(left:35.0,top: 50.0),
 
-          child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w300,
-              fontStyle: FontStyle.italic,
-              color: Colors.white
-
-            ),
-
-            )
-    )
-    );
+            child: Row(
+              children: <Widget>[
+                Expanded( child: Text(
+                  "Spice jet",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                )),
+                Expanded(child:Text(
+                  "From Mumbai to Bangalore via New Delhi",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                )),
+              ],
+            )));
   }
 }
