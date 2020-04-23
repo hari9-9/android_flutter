bcf1cb0fa6428d0f971fb7d3e6eda970dfca76f4
hari9_9 <reachk.hariharan@gmail.com>
2020-04-23 20:08:58 +0530

    adding alert using Raised button

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget {
 
     return Center(
         child: Container(
-            padding: EdgeInsets.only(left:10.0, top:40.0),
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
             alignment: Alignment.center,
             color: Colors.deepPurple,
             //width: 200.0,
@@ -72,7 +72,8 @@ class Home extends StatelessWidget {
                     )),
                   ],
                 ),
-                FlightImageAsset()
+                FlightImageAsset(),
+                FlightBookButton()
               ],
             )
 /*             child: Row(
@@ -105,13 +106,50 @@ class Home extends StatelessWidget {
   }
 }
 
-class FlightImageAsset extends StatelessWidget
-{
+class FlightImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    AssetImage assetImage=AssetImage('images/flight.png');
-    Image image=Image(image:assetImage,width: 250.0,height:250.0 ,);
-    return Container(child: image,);
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(
+      child: image,
+    );
   }
-  
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+            "Book Your Flight",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w300),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookFlight(context)),
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked Sucessfully"),
+      content: Text("Have a pleasant flight"),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
+  }
 }
