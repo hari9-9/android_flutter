372312608968d6541167e11b4a13185e72814623
hari9_9 <reachk.hariharan@gmail.com>
2020-04-23 16:33:32 +0530

    adding rows and columns with some padding

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -8,13 +8,73 @@ class Home extends StatelessWidget {
 
     return Center(
         child: Container(
+            padding: EdgeInsets.only(left:10.0, top:40.0),
             alignment: Alignment.center,
             color: Colors.deepPurple,
             //width: 200.0,
             //height: 100.0,
             //margin:EdgeInsets.only(left:35.0,top: 50.0),
-
-            child: Row(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Spice jet",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "From Mumbai to Bangalore via New Delhi",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Air India",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "From Jaipur to Goa",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white),
+                    )),
+                  ],
+                )
+              ],
+            )
+/*             child: Row(
               children: <Widget>[
                 Expanded( child: Text(
                   "Spice jet",
@@ -39,6 +99,7 @@ class Home extends StatelessWidget {
                       color: Colors.white),
                 )),
               ],
-            )));
+            ) */
+            ));
   }
 }
