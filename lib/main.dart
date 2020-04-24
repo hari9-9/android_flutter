2676f9a57c3cb56cd22fd40a5037d425e9551608
hari9_9 <reachk.hariharan@gmail.com>
2020-04-24 18:45:14 +0530

    floating action button and snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,53 @@
 import 'package:flutter/material.dart';
 import './app_screens/home.dart';
-void main()
-{
-  runApp(MaterialApp(
-      title:"Exploring UI widgets",
-      home:Scaffold(
-        appBar: AppBar(title: Text("Long List View"),),
-        body:getListView()
-      ),
 
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    title: "Exploring UI widgets",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Long List View"),
+      ),
+      body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FAB clicked");
+        },
+        child: Icon(Icons.add),
+        tooltip: "Add one more Item",
+      ),
+    ),
+  ));
 }
 
-List<String> getListElements(){
-  var items=List<String>.generate(1000, (counter)=>"Item $counter");
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text("You just tapped $item"),
+    action: SnackBarAction(
+        label: "UNDO",
+        onPressed: () {
+          debugPrint("Dummy undo operation");
+        }),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
   return items;
 }
 
-Widget getListView()
-{
+Widget getListView() {
   var listItems = getListElements();
-  var listView = ListView.builder(
-    itemBuilder: (context,index){
-      return ListTile(
-        leading:Icon(Icons.arrow_right) ,
-
-        title:Text(listItems[index]) ,
-        onTap: (){
-          debugPrint("${listItems[index]} was tapped");
-        },
-      );
-    }
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+      onTap: () {
+        //debugPrint("${listItems[index]} was tapped");
+        showSnackBar(context, listItems[index]);
+      },
     );
+  });
   return listView;
 }
-
