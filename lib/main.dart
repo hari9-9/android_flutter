2cb8eee9b68089855463ea7a7f1a89eb3e0543b0
hari9_9 <reachk.hariharan@gmail.com>
2020-04-24 17:59:33 +0530

    Implemented Long List view

--- lib/main.dart
+++ lib/main.dart
@@ -5,39 +5,34 @@ void main()
   runApp(MaterialApp(
       title:"Exploring UI widgets",
       home:Scaffold(
-        appBar: AppBar(title: Text("Basic List View"),),
-        body: getListView(),
+        appBar: AppBar(title: Text("Long List View"),),
+        body:getListView()
       ),
 
     )
   );
 }
 
+List<String> getListElements(){
+  var items=List<String>.generate(1000, (counter)=>"Item $counter");
+  return items;
+}
+
 Widget getListView()
 {
-  var listView=ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: (){
-          debugPrint("Landscape tapped");
-        },
+  var listItems = getListElements();
+  var listView = ListView.builder(
+    itemBuilder: (context,index){
+      return ListTile(
+        leading:Icon(Icons.arrow_right) ,
 
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone_android),
-        title: Text("Phone"),
-      ),
-      Text("One more element"),
-      Container(color:Colors.black,height:50.0)
-    ],
-  );
+        title:Text(listItems[index]) ,
+        onTap: (){
+          debugPrint("${listItems[index]} was tapped");
+        },
+      );
+    }
+    );
   return listView;
 }
+
