be870471948dd42c40066de14442b93711f43923
hari9_9 <reachk.hariharan@gmail.com>
2020-04-27 19:40:39 +0530

    note list screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:first_build/screens/note_list.dart';
+
+void main() {
+  runApp(MyApp());
+  
+}
+
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title:'Note keeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch:Colors.deepPurple,
+
+      ),
+      home: NoteList(),
+      
+    );
+  }
+  
+}
